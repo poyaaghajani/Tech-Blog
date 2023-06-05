a4fbc11059b165b5bbb76447ba9b8bbcb0273136
poya <poya1381@gmail.com>
2023-06-05 20:06:41 +0330

    add: my http overrides file to set certificate

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tech_blog/component/my_colors.dart';
+import 'package:tech_blog/my_http_overrides.dart';
 import 'package:tech_blog/themes/app_theme.dart';
 import 'package:tech_blog/view/splash_screen.dart';
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: SolidColors.statusBarColor,
